2de90f6869a4b132e3b8f0772611be3da6fba0b3
NCenVictoria <122835761+NCenVictoria@users.noreply.github.com>
2023-07-12 15:10:48 -0600

    19

--- server/src/database.js
+++ server/src/database.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { DB_URL , DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 const fs = require("fs");
 const path = require("path");
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  `${DB_URL}`,
   { logging: false },
 );
 
